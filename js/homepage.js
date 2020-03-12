2f7e600885cd25c7cfbebb3b276f2a653e5105a3
ebwinters@comcast.net <ebwinters@comcast.net>
2020-03-11 20:40:14 -0400

    homepage and grid work beginning

--- js/homepage.js
+++ js/homepage.js
@@ -1,2 +1,12 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+/**
+ * Navigates to a page, but first waits 75ms and briefly gives visual feedback (color change) on click
+ * @param {} div will be self
+ * @param {string} pageName local path to file ex: "index.html"
+ */
+function selectChoice(div, pageName) {
+    div.style.background="#f9e572";
+    setTimeout(function() {
+        window.location.href=pageName;
+    }, 75); 
+}
+
