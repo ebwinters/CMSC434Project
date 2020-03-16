d580026a7f7ae6e984f08fbe8713a05a8b2bec0a
ebwinters@comcast.net <ebwinters@comcast.net>
2020-03-16 09:38:29 -0400

    set up visitor homepage

--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,2 @@
-/**
- * Navigates to a page, but first waits 75ms and briefly gives visual feedback (color change) on click
- * @param {} div will be self
- * @param {string} pageName local path to file ex: "index.html"
- */
-function selectChoice(div, pageName) {
-    div.style.background="#f9e572";
-    setTimeout(function() {
-        window.location.href=pageName;
-    }, 75); 
-}
+
 
