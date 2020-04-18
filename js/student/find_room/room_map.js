c99e93b1ac97cd1ee3860eaa36b94c3d8acd7c59
ebwinters@comcast.net <ebwinters@comcast.net>
2020-04-18 17:37:42 -0500

    tap to enlarge

--- js/student/find_room/room_map.js
+++ js/student/find_room/room_map.js
@@ -3,4 +3,22 @@ function setHead() {
     document.getElementById('head').innerText = `Directions to ${roomNum}`;
 }
 
+function enlarge(imgOption) {
+    document.getElementById('grid-container').style = "display: none;";
+    document.getElementById('exit-btn').style = "display: flex;";
+    if (imgOption === 'lower') {
+        document.getElementById('low-hide').style = "display: flex;";
+    }
+    else {
+        document.getElementById('up-hide').style = "display: flex;";
+    }
+}
+
+function restore() {
+    document.getElementById('low-hide').style = "display: none;";
+    document.getElementById('up-hide').style = "display: none;";
+    document.getElementById('exit-btn').style = "display: none;";
+    document.getElementById('grid-container').style = "display: grid;";
+}
+
 window.onload = setHead();
